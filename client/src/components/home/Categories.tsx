abfbe6354d4b56f5537da7fb296034a9f6bdbaf6
Aditya Shiyale <adityashiyale7219@gmail.com>
2025-11-27 18:18:38 +0530

    update category objects, add new category page UI, and improve home navigation

--- client/src/components/home/Categories.tsx
+++ client/src/components/home/Categories.tsx
@@ -1,8 +1,16 @@
 import { Text, FlatList, Image, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { categories } from "@/src/constants/categories";
+import { UnknownInputParams, router } from "expo-router";
 
 const Categories = () => {
+  const handleCategory = (category: UnknownInputParams) => {
+    router.push({
+      pathname: "/categories/[category]",
+      params: category,
+    });
+  };
+
   return (
     <View>
       <View className="mb-3 w-full flex-row items-center justify-between">
@@ -13,10 +21,17 @@ const Categories = () => {
       </View>
       <FlatList
         data={categories}
+        keyExtractor={(item) => item.id.toString()}
         horizontal
         contentContainerStyle={{ gap: 15 }}
-        renderItem={({ index, item }) => (
-          <TouchableOpacity activeOpacity={0.2} className="items-center gap-1">
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() =>
+              handleCategory({ category: item.category } as UnknownInputParams)
+            }
+            activeOpacity={0.2}
+            className="items-center gap-1"
+          >
             <Image
               source={item.image}
               className="size-20 rounded-full bg-violet-400"
